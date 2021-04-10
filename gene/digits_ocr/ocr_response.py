da0c1ed26acc548583ab978e3d3fc8f8abfc33ac
anoyo-lin <anoyo.lin@gmai.com>
2021-04-10 20:11:19 +0800

    [GENE-6] chmod of vscode

--- gene/digits_ocr/ocr_response.py
+++ gene/digits_ocr/ocr_response.py
@@ -5,7 +5,7 @@ import numpy as np
 from gene.digits_ocr.utils import sort_by_confidence_then_filter_top_n
 
 # OCRResponse has 3 attributes
-# - predirction_results
+# - prediction_results
 # - confidence_threshold
 # - top_N
 class OCRResponse(ABC):
@@ -13,6 +13,7 @@ class OCRResponse(ABC):
         self.prediction_results = prediction_results
         self.confidence_threshold = confidence_threshold
         self.top_n = top_n
+
 # MNIST text annotations has 2 attributes
 # - prediction
 # - confidence
@@ -20,6 +21,29 @@ class MNISTTextAnnotations(object):
     def __init__(self, prediction_results, confidence_threshold: float = 0.6):
         self.prediction = str(np.argmax(prediction_results))
         self.confidence = np.max(prediction_results)
+
+class RowNumbersTextAnnotations(object):
+    def __init__(self, prediction_results, confidence_threshold: float = 0.6):
+        result_position_below_threshold = np.argwhere(
+            np.max(prediction_results, axis=1) < confidence_threshold
+        ).flatten()
+        prediction = np.argmax(prediction_results, axis=1).astype(str)
+        description = prediction.copy()
+        # mask the char by '*' if below threshold
+        if len(result_position_below_threshold) != 0:
+            for arg_pos in result_position_below_threshold:
+                description[arg_pos] = '*'
+        self.description = ''.join(description)
+        self.prediction = ''.join(prediction)
+        self.confidence = np.mean(np.flip(np.sort(prediction_results)[:, -1:], 1))
+
+# FullTextAnnotations
+# - stringify all result in result_stack
+# - dict { str(key): float(value)}
+class FullTextAnnotations(object):
+    def __init__(self, order, result):
+        self.order = order
+        self.top_n = {str(int(arg)): float(round(val, 5)) for arg, val in result}
 
 # MNIST OCR Response
 # will extend OCRResponse and has extra textAnnotations and fullTextAnnotations
@@ -35,10 +59,10 @@ class MNISTOCRResponse(OCRResponse):
         result_stack = sort_by_confidence_then_filter_top_n(prediction_results, top_n)
         self.fullTextAnnotations = [FullTextAnnotations(idx, result) for idx, result in enumerate(result_stack)]
 
-# FullTextAnnotations
-# - stringify all result in result_stack
-# - dict { str(key): float(value)}
-class FullTextAnnotations(object):
-    def __init__(self, order, result):
-        self.order = order
-        self.top_n = {str(int(arg)): float(round(val, 5)) for arg, val in result}
+class RowNumbersOCRResponse(OCRResponse):
+    def __init__(self, prediction_results, confidence_threshold: float = 0.6, top_n: int = 3):
+        super(RowNumbersOCRResponse, self).__init__(prediction_results, confidence_threshold, top_n)
+        self.textAnnotations =  RowNumbersTextAnnotations(prediction_results, confidence_threshold)
+        result_stack = sort_by_confidence_then_filter_top_n(prediction_results, top_n)
+
+        self.fullTextAnnotations = [FullTextAnnotations(idx, result) for idx, result in enumerate(result_stack)]
--- gene/digits_ocr/serializers.py
+++ gene/digits_ocr/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from gene.ocr_config import cfg
 
+class TextAnnotationsSerializer(serializers.Serializer):
+    description = serializers.CharField()
+    prediction = serializers.CharField()
+    confidence = serializers.FloatField()
 # - prediction CHAR
 # - confidence FLOAT
 class MNISTTextAnnotationsSerializer(serializers.Serializer):
@@ -11,10 +15,16 @@ class MNISTTextAnnotationsSerializer(serializers.Serializer):
 class FullTextAnnotationsSerializer(serializers.Serializer):
     order = serializers.IntegerField()
     top_n = serializers.DictField()
+
 # - textAnnotation MNISTTextAnnotation
 # - fullTextAnnotations LIST of FullTextAnnotations
 class MNISTOCRResponseSerializer(serializers.Serializer):
     textAnnotations = MNISTTextAnnotationsSerializer()
+    fullTextAnnotations = serializers.ListField(
+        child=FullTextAnnotationsSerializer()
+    )
+class RowNumbersOCRResponseSerializer(serializers.Serializer):
+    textAnnotations = TextAnnotationsSerializer()
     fullTextAnnotations = serializers.ListField(
         child=FullTextAnnotationsSerializer()
     )
@@ -42,4 +52,22 @@ class MNISTOCRRequestSerializer(serializers.Serializer):
         if 'image' not in value.content_type:
             raise serializers.ValidationError("Only image files are supported")
         return value
-                                                  
+class RowNumbersOCRResponseRequestSerializer(serializers.Serializer):
+    image = serializers.FileField(required=True)
+    confidence_threshold = serializers.FloatField(
+        min_value=cfg.API.MNIST.CONFIDENCE_THRESHOLD.MIN,
+        max_value=cfg.API.MNIST.CONFIDENCE_THRESHOLD.MAX,
+        default=cfg.API.MNIST.CONFIDENCE_THRESHOLD.DEFAULT,
+        required=False
+    )
+
+    top_n = serializers.IntegerField(
+        min_value=cfg.API.MNIST.TOP_N.MIN,
+        max_value=cfg.API.MNIST.TOP_N.MAX,
+        default=cfg.API.MNIST.TOP_N.DEFAULT,
+        required=False
+    )
+    def validate_image(self, value):
+        if 'image' not in value.content_type:
+            raise serializers.ValidationError("Only image files are supported")
+        return value
